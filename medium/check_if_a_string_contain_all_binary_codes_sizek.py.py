43d3d4ce35e9ce493e728358a9400bbac0416ec2
d1zm4as <pedrofamilia364@gmail.com>
2026-02-23 08:35:10 -0300

    +2

---
+++ medium/check_if_a_string_contain_all_binary_codes_sizek.py.py
@@ -0,0 +1,6 @@
+class Soluttion:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        seen = set()
+        for i in range(len(s) - k + 1):
+            seen.add(s[i:i+k])
+        return len(seen) == 2 ** k
